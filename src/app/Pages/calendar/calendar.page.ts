9fa9c120cffa955fe4235c0ae78b49d9668b510c
Lucas <52681405+lucas-llc@users.noreply.github.com>
2023-03-20 22:09:50 -0300

    :rocket: Tela de calendário de pagamentos

--- src/app/Pages/calendar/calendar.page.ts
+++ src/app/Pages/calendar/calendar.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignatureService } from 'src/app/services/signature/signature.service';
+import { UtilService } from 'src/app/services/util/util.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: 'calendar.page.html',
   styleUrls: ['calendar.page.scss']
 })
-export class CalendarPage {
+export class CalendarPage implements OnInit {
+  loading = false;
+  signatureMonthList: any;
+  empty = false;
+  constructor(
+    public ss: SignatureService,
+    public util: UtilService
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.getCalendar();
+  }
 
+  getCalendar() {
+    this.loading = true;
+    this.ss.getCalendar().subscribe({
+      next: (data) => {
+        this.signatureMonthList = data;
+        if (this.signatureMonthList?.length > 0) {
+          this.empty = false;
+        } else {
+          this.empty = true;
+        }
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
